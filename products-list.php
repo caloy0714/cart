61007f28da92d09d77acf6132a64e29754c90e40
Carlitos Bernardino <carlitosbernardino@Carlitoss-MacBook-Pro.local>
2022-03-22 17:17:05 +0800

    Customized the design, added my own logo, updated the layout of products, and shopping cart

--- products-list.php
+++ products-list.php
@@ -10,20 +10,24 @@ $products = Product::convertArrayToProducts($products_data);
 $customer = new Customer('John Doe', 'john@mail.com');
 ?>
 <html>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <head>
+<div class="container-fluid">
+    <img class="navbar-brand" src="https://toppng.com/uploads/preview/shopping-cart-png-image-shopping-cart-icon-sv-11562865326ta92uix1ak.png" height="54" width="40" padding="2px,10px" class="d-inline-block align-text-top">
     <title>My Cart</title>
+</div>
 </head>
-<body>
-
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Products</h2>
+<body style="background: url(https://img1.wallspic.com/previews/1/7/1/7/5/157171/157171-entertainment-art-arm-wheel-shopping_cart-x350.jpg) no-repeat; background-size: 100% 100%;">
+<h1 class="text-center">Welcome <?php echo $customer->getName() ?>!</h1>
+<h2 class="text-center">Products</h2>
 <h4>
     <a href="shopping-cart.php">Shopping Cart</a>
 </h4>
 
 <?php foreach ($products as $product): ?>
 
-<form action="add-to-cart.php" method="POST">
+<form action="add-to-cart.php" method="POST" class="text-center">
     <input type="hidden" name="product_id" value="<?php echo $product->getId(); ?>" />
     <h3><?php echo $product->getName(); ?></h3>
     <img src="<?php echo $product->getImage(); ?>" height="100px" />
--- shopping-cart.php
+++ shopping-cart.php
@@ -14,16 +14,18 @@ $shoppingCart = new ShoppingCart($customer);
 $shoppingCartItems = $shoppingCart->getAllItems();
 ?>
 <html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <head>
     <title>My Cart</title>
 </head>
-<body>
+<body style="background: url(https://img1.wallspic.com/previews/1/7/1/7/5/157171/157171-entertainment-art-arm-wheel-shopping_cart-x350.jpg) no-repeat; background-size: 100% 100%;">
+<div class="container-fluid">
+        <img class="navbar-brand" src="https://toppng.com/uploads/preview/shopping-cart-png-image-shopping-cart-icon-sv-11562865326ta92uix1ak.png" height="54" width="40" padding="2px,10px" class="d-inline-block align-text-top">
+            <h1 class="text-center">Welcome <?php echo $customer->getName() ?>!</h1></br>
+            <h2 class="text-center">Shopping Cart is the brand new online shopping cart! <a href="products-list.php">Click Here to Shop More Products</a></h2>
+</div>
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Shopping Cart</h2>
-<h4>
-    <a href="products-list.php">Shop More Products</a>
-</h4>
+
 
 <?php if (count($shoppingCartItems) > 0): ?>
 
